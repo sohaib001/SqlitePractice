ab00363393a4b4dca16a371b963993efa85f4a62
Sohaib Muhammad <sohaib.muhammad@coeus-solutions.de>
2013-03-15 12:33:10 +0500

    Initial Commit

---
+++ SqlitePractice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SqlitePractice
+//
+//  Created by Sohaib Muhammad on 15/03/2013.
+//  Copyright (c) 2013 coeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
